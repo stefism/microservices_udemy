8655a84776f3993cb9494fbe791387a8764745c4
Stefan Markov <stef4otm@gmail.com>
2024-01-09 16:17:07 +0200

    Add coupons view

---
+++ Mango/Mango.Web/Controllers/CouponController.cs
@@ -0,0 +1,30 @@
+﻿using Mango.Web.Models;
+using Mango.Web.Service.IService;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+namespace Mango.Web.Controllers
+{
+    public class CouponController : Controller
+    {
+        private readonly ICouponService _couponService;
+
+        public CouponController(ICouponService couponService)
+        {
+            _couponService = couponService;
+        }
+
+        public async Task<IActionResult> CouponIndex()
+        {
+            List<CouponDto> coupons = new();
+            ResponseDto? response = await _couponService.GetAllCouponsAsync();
+            
+            if (response != null && response.IsSuccess)
+            {
+                coupons = JsonConvert.DeserializeObject<List<CouponDto>>(Convert.ToString(response.Result));
+            }
+
+            return View(coupons);
+        }
+    }
+}
