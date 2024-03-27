7196fb82d76f72992409bfbd4dbea993d427506a
Stefan Markov <stef4otm@gmail.com>
2024-03-27 15:13:42 +0200

    Add Gateway project

---
+++ Mango/Mango.GatewaySolution/Program.cs
@@ -0,0 +1,14 @@
+using Ocelot.DependencyInjection;
+using Ocelot.Middleware;
+
+var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddOcelot();
+
+var app = builder.Build();
+
+
+app.MapGet("/", () => "Hello World!");
+
+app.UseOcelot();
+
+app.Run();
