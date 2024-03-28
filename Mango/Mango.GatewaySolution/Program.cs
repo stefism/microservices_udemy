07641f125d0f24e6be25e056aec9af512a70fdde
Stefan Markov <stef4otm@gmail.com>
2024-03-28 17:34:51 +0200

    Add routes to gateway

--- Mango/Mango.GatewaySolution/Program.cs
+++ Mango/Mango.GatewaySolution/Program.cs
@@ -4,13 +4,16 @@ using Ocelot.Middleware;
 
 var builder = WebApplication.CreateBuilder(args);
 builder.AddAppAuthentication();
-builder.Services.AddOcelot();
+
+builder.Configuration.AddJsonFile("ocelot.json", optional: false, reloadOnChange: true);
+
+builder.Services.AddOcelot(builder.Configuration);
 
 var app = builder.Build();
 
 
 app.MapGet("/", () => "Hello World!");
 
-app.UseOcelot();
+app.UseOcelot().GetAwaiter().GetResult();
 
 app.Run();
